3cc39a0c44c8dcc8419635e21ff762b6aa268720
myaminaye <myaminaye22@gmail.com>
2023-06-20 11:40:12 +0630

    api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,94 +1,96 @@
-// import axios from "axios";
+import axios from "axios";
 import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ForecastCard from "./ForecastCard";
 import './styles/weather.css'
 
-export default function Weather() {
-    // const apiKey = "AIzaSyA9Zz5oJEqKPUOCvPbZiiyKf74vEj0C5j8";
-    // const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?lat=44.34&lon=10.99&key=${apiKey}`;
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false })
 
-    const [city, setCity] = useState("Myanmar");
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            coordinates: response.data.coord,
+            temperature: Math.round(response.data.main.temp),
+            humidity: response.data.main.humidity,
+            date: "Friday 5:00PM",
+            description: response.data.weather[0].description,
+            icon: response.data.weather[0].icon,
+            wind: response.data.wind.speed,
+            city: response.data.name,
+        });
+    }
+
+
+    
 
     const handleSearch = (event) => {
         event.preventDefault();
         const searchValue = event.target.elements.search.value;
-        setCity(searchValue);
+        setWeatherData({city: searchValue})
     };
 
-    // function formatDate(timestamp) {
-    //     let date = new Date(timestamp);
-    //     let hours = date.getHours();
-    //     if (hours < 10) {
-    //         hours = `0${hours}`
-    //     }
-    //     let minutes = date.getMinutes();
-    //     if (minutes < 10) {
-    //         minutes = `0${minutes}`
-    //     }
-    //     let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    //     let day = days[date.getDay()];
-    //     return `${day} ${hours}:${minutes}`
-    // }
-
-    // function formatDay(timestamp) {
-    //     let date = new Date(timestamp * 1000);
-    //     let day = date.getDay();
-    //     let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-    //     return days[day];
-    // }
 
     const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-    return (
-        <div className="root">
-            <h2 className="title">Weather Application by Emerald</h2>
-            <div className="main">
-                <ul className="savedCities">
-                    <li className="savedCity">Japan</li>
-                    <li className="savedCity">London</li>
-                    <li className="savedCity">Thailand</li>
-                    <li className="savedCity">Korea</li>
-                </ul>
-                <div className="row" style={{ padding: "0 30px" }}>
-                    <div className="col-6" >
-                        <form onSubmit={handleSearch} style={{ display: "flex" }}>
-                            <input placeholder="Enter a city"  name="search" />
-                            <button type="submit" variant="contained" className="searchBtn">Search</button>
-                        </form>
+    if (weatherData.ready) {
+        return (
+            <div className="root">
+                <h2 className="title">Weather Application by Emerald</h2>
+                <div className="main container">
+                    <ul className="savedCities">
+                        <li className="savedCity">Japan</li>
+                        <li className="savedCity">London</li>
+                        <li className="savedCity">Thailand</li>
+                        <li className="savedCity">Korea</li>
+                    </ul>
+                    <div className="row" style={{ padding: "0 30px" }}>
+                        <div className="col-6" >
+                            <form onSubmit={handleSearch} style={{ display: "flex" }}>
+                                <input placeholder="   Enter a city" name="search" className="rounded border" />
+                                <button type="submit" className="btn btn-primary searchBtn">Search</button>
+                            </form>
+                        </div>
+                        <div className="col-6 d-flex justify-content-end" >
+                            <button className="btn btn-primary currentBtn ">Current</button>
+                        </div>
                     </div>
-                    <div className="col-6" >
-                        <button variant="contained" className="currentBtn">Current</button>
+                    <div className="justify-content-start">
+                        <h1 className="lh-lg">{weatherData.city}</h1>
+                        <p>{weatherData.date}</p>
+                        <p className="text-capitalize">{weatherData.description}</p>
+                    </div>
+                    <div className="row m-5 d-flex justify-content-center" >
+                        <div className="col-4 d-flex">
+                            <img src="http://openweathermap.org/img/wn/04d@2x.png" style={{ marginTop: "-50px" }} alt="weather status icon" />
+                            <h3>{weatherData.temperature}°C</h3>
+                        </div>
+                        <div className="col-4">
+                            <p>Humidity: {weatherData.humidity}%</p>
+                            <p>Wind: {weatherData.wind}km/h</p>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center my-2" >
+                        {
+                            days.map((day, index) => (
+                                <div key={index} className="col-1 mx-3 text-center">
+                                    <ForecastCard day={day} temp="18" />
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
-                <div>
-                    <p>{city}</p>
-                    <p>Sunday 10:48</p>
-                    <p>Clouds</p>
-                </div>
-                <div className="row" >
-                    <div className="col-6 d-flex">
-                        <img src="http://openweathermap.org/img/wn/04d@2x.png" style={{marginTop: "-50px"}} alt="weather status icon"/>
-                        <h3>19'C</h3>
-                    </div>
-                    <div className="col-6">
-                        <p>Precipitation: 85%</p>
-                        <p>Wind: 4km/h</p>
-                    </div>
-                </div>
-                <div className="row justify-content-center my-2" >
-                    {
-                        days.map((day, index) => (
-                            <div key={index} className="col-1 mx-3 text-center">
-                                <ForecastCard day={day} temp="18" />
-                            </div>
-                        ))
-                    }
-                </div>
-
+                <footer>
+                    <small className="my-3"><a href="https://github.com/myaminaye/weather-react" rel="noreferrer" target="_blank">Open-source code</a>, by <a href="https://myaminaye.netlify.app/" rel="noreferrer" target="_blank" style={{ color: "black", textDecoration: "none" }}>Mya Min Aye</a></small>
+                </footer>
             </div>
-            <small><a href="https://github.com/myaminaye/weather-react-website">Open-source code</a>, by Mya Min Aye</small>
-        </div>
-    )
+        )
+    }
+    else {
+        const apiKey = "085471793943e047acaad88b8e636154";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+        return "Loading...";
+    }
 }
